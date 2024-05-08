49edb37e3f382738080b8fe5a82e2b685645608a
Manuel Zeballos <manuel.zeballos@ig.edu.ar>
2024-05-08 16:06:39 -0300

    Versión 2 de eje2.sh y de eje3.sh

--- eje2.sh
+++ eje2.sh
@@ -1,1 +1,2 @@
+#! /bin/bash
 echo "Me llamo $USER"
---
+++ eje3.sh
@@ -0,0 +1,4 @@
+#Haga un programa en bash, donde usted debe pasar los argumentos de su nombre
+#y edad y el programa los imprime en pantalla
+edad=28
+echo "Me llamo $USER y tengo $edad años"
