4639644153aea50a883908607669818c46ae8a06
Manuel Zeballos <manuel.zeballos@ig.edu.ar>
2024-05-13 15:12:09 -0300

    eje11.sh version 1 y py de eje11

--- eje11.sh
+++ eje11.sh
@@ -1,7 +1,6 @@
-"#Desafío: Cree un programa en bash que lea la cantidad de bytes que recibe su placa de red (ver el archivo /sys/class/net/enpXsX/statistics), cada un
-#segundo, y lo guarde en el archivo /tmp/datos. El archivo debe contenener, en cada fila, primero la hora, y separado por un espacio, el tráfico recibido.
-#Luego, debe procesar ese archivo para graficar el tráfico de red. Dispone del
-#siguiente script en python para tal fin Desafío: Cree un programa en bash que lea la cantidad de bytes que recibe su placa
-#de red (ver el archivo /sys/class/net/enpXsX/statistics), cada un
-#segundo, y lo guarde en el archivo /tmp/datos. El archivo debe contenener, en cada fila, primero la hora, y separado por un espacio, el tráfico recibido.
-#Luego, debe procesar ese archivo para graficar el tráfico de red. Dispone del siguiente script en python para tal fin
+#Desafío: Cree un programa en bash que lea la cantidad de bytes que recibe su placa de red
+#(ver el archivo /sys/class/net/enpXsX/statistics), cada un segundo, y lo guarde en el archivo /tmp/datos.
+#El archivo debe contenener, en cada fila, primero la hora, y separado por un espacio, el tráfico recibido.
+#Luego, debe procesar ese archivo para graficar el tráfico de red.
+
+less /sys/class/net/enp2s2/statistics/rx_bytes
