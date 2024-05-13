b4f1abab72f3a37bc6522122f8f12ba453989a57
Manuel Zeballos <manuel.zeballos@ig.edu.ar>
2024-05-13 10:15:04 -0300

    version final eje10.sh

--- eje10.sh
+++ eje10.sh
@@ -1,7 +1,5 @@
 #! /bin/bash
 #Imprima la RAM de su máquina
-#Memoria=$(sudo dmidecode --type memory)
-#Capacidad=$($Memoria | grep 'Maximum Capacity')
-#echo ("Capacidad máxima de la RAM:" $(sudo dmidecode --type memory))
+
 echo "Capacidad máxima de la RAM:"
 sudo dmidecode --type memory| grep -oP 'Maximum Capacity: \K[0-9]+'
